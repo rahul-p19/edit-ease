768083531c95c9fbbd959c631c91a5851df97d66
rahul-p19 <pandeyrahul.work@gmail.com>
2025-07-28 10:52:40 +0530

    fix: eslint build error

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
-
 function NotFound() {
   return (
-    <div className='flex flex-col items-center gap-y-3'>
-        <h1 className='text-8xl font-bold'>404</h1>
-        <h2 className='text-4xl font-medium'>Page Not Found</h2>
+    <div className="flex flex-col items-center gap-y-3">
+      <h1 className="text-8xl font-bold">404</h1>
+      <h2 className="text-4xl font-medium">Page Not Found</h2>
     </div>
-  )
+  );
 }
 
-export default NotFound
+export default NotFound;
