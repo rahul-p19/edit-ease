4e601f8930570a346105067e0d19d61e5949309a
rahul-p19 <pandeyrahul.work@gmail.com>
2025-07-28 10:51:15 +0530

    refactor: separate backend and frontend

--- backend/src/main/java/tech/rahulpandey/backend/repository/MailListRepository.java
+++
@@ -1,9 +0,0 @@
-package tech.rahulpandey.backend.repository;
-
-import org.springframework.data.domain.Limit;
-import org.springframework.data.repository.CrudRepository;
-import tech.rahulpandey.backend.model.MailList;
-
-public interface MailListRepository extends CrudRepository<MailList, String> {
-    MailList findByEmail(String email);
-}
